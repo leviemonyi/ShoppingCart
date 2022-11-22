447fa0bc6cc00b75442f81d88944f20fce0c205c
leviemonyi <98327069+leviemonyi@users.noreply.github.com>
2022-11-22 21:09:19 +0300

    Add files via upload

---
+++ mini-project-of-shopping-cart-main/products.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    title: "Product 1",
+    price: "$ 239.99",
+    imgUrl: "./assets/images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    price: "$ 339.99",
+    imgUrl: "./assets/images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "Product 3",
+    price: "$ 129.99",
+    imgUrl: "./assets/images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "Product 4",
+    price: "$ 459.99",
+    imgUrl: "./assets/images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "Product 5",
+    price: "$ 349.99",
+    imgUrl: "./assets/images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "Product 6",
+    price: "$ 799.99",
+    imgUrl: "./assets/images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "Product 7",
+    price: "$ 899.99",
+    imgUrl: "./assets/images/product-7.jpeg",
+  },
+  {
+    id: 8,
+    title: "Product 8",
+    price: "$ 199.99",
+    imgUrl: "./assets/images/product-8.jpeg",
+  },
+];
